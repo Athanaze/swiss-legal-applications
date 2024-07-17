fdeb8cf1ae1b9956bbe2a76f03629ab6f375b10c
Athanaze <sacha.liechti@arsent.ch>
2024-07-17 19:38:18 +0200

    a

---
+++ anonymization/gen_ch_names_matrix.py
@@ -0,0 +1,122 @@
+# Array of 50 male French names
+french_male_names = [
+"Jean", "Pierre", "Michel", "Philippe", "André", "Louis", "François", "Alain", "Jacques", "Bernard",
+    "Marcel", "René", "Paul", "Robert", "Henri", "Claude", "Daniel", "Christian", "Georges", "Roger",
+    "Joseph", "Maurice", "Charles", "Guy", "Raymond", "Gérard", "Yves", "Patrick", "Serge", "Lucien",
+    "Émile", "Fernand", "Julien", "Gaston", "Albert", "Léon", "Étienne", "Gilbert", "Gilles", "Olivier",
+    "Pascal", "Thierry", "Laurent", "Nicolas", "Stéphane", "Frédéric", "Éric", "Denis", "Christophe", "Vincent",
+    "Antoine", "Benoît", "Cédric", "Damien", "Dominique", "Édouard", "Fabrice", "Grégoire", "Hugo", "Igor",
+    "Jérôme", "Kévin", "Ludovic", "Maxime", "Nicolas", "Olivier", "Pierre", "Quentin", "Raphaël", "Sébastien",
+    "Thibault", "Ulysse", "Victor", "William", "Xavier", "Yann", "Zacharie", "Adrien", "Baptiste", "Clément",
+    "Dylan", "Éliott", "Florian", "Gaël", "Hervé", "Ilan", "Jules", "Kilian", "Léo", "Mathieu",
+    "Nathan", "Oscar", "Paul", "Quentin", "Romain", "Simon", "Théo", "Ugo", "Valentin", "Willy",
+    "Alexandre", "Bastien", "Cyril", "David", "Émeric", "Fabien", "Gabin", "Hippolyte", "Isidore", "Jean-Baptiste",
+    "Kylian", "Lilian", "Malo", "Nolan", "Octave", "Philippe", "Rémi", "Sacha", "Timothée", "Urbain",
+    "Vianney", "Wilfried", "Xander", "Yvan", "Zéphyr", "Arnaud", "Bruno", "César", "Dimitri", "Émeric",
+    "Félix", "Gauthier", "Hadrien", "Ibrahim", "Johan", "Kenny", "Lorenzo", "Marius", "Noham", "Owen",
+    "Pablo", "Quincy", "Rayan", "Samuel", "Tanguy", "Ugo", "Vasco", "Warren", "Xavi", "Yannis"
+]
+# Array of 50 female French names
+french_female_names = [
+    "Marie", "Jeanne", "Françoise", "Monique", "Catherine", "Nathalie", "Isabelle", "Sylvie", "Anne", "Martine",
+    "Jacqueline", "Nicole", "Sophie", "Chantal", "Suzanne", "Christiane", "Yvonne", "Madeleine", "Céline", "Véronique",
+    "Marguerite", "Christine", "Simone", "Thérèse", "Odette", "Denise", "Geneviève", "Hélène", "Brigitte", "Valérie",
+    "Danielle", "Josette", "Paulette", "Renée", "Colette", "Claudine", "Micheline", "Marcelle", "Éliane", "Andrée",
+    "Stéphanie", "Patricia", "Sandrine", "Dominique", "Laurence", "Caroline", "Élisabeth", "Aurélie", "Émilie", "Julie",
+    "Camille", "Claire", "Lucie", "Mathilde", "Alice", "Juliette", "Léa", "Manon", "Pauline", "Anaïs",
+    "Élodie", "Sarah", "Laura", "Marion", "Océane", "Charlotte", "Audrey", "Mélanie", "Justine", "Amélie",
+    "Adeline", "Delphine", "Virginie", "Laetitia", "Eloïse", "Cécile", "Estelle", "Morgane", "Amandine", "Inès",
+    "Clémence", "Alexia", "Léna", "Zoé", "Elsa", "Noémie", "Maëlle", "Romane", "Chloé", "Fanny",
+    "Agathe", "Léonie", "Louise", "Jade", "Emma", "Margaux", "Solène", "Mélissa", "Coralie", "Marine"
+]
+# Array of 50 French family names
+french_family_names = [
+    "Martin", "Bernard", "Dubois", "Thomas", "Robert", "Richard", "Petit", "Durand", "Leroy", "Moreau",
+    "Simon", "Laurent", "Lefebvre", "Michel", "Garcia", "David", "Bertrand", "Roux", "Vincent", "Fournier",
+    "Morel", "Girard", "Andre", "Lefevre", "Mercier", "Dupont", "Lambert", "Bonnet", "Francois", "Martinez",
+    "Legrand", "Garnier", "Faure", "Rousseau", "Blanc", "Guerin", "Muller", "Henry", "Roussel", "Nicolas",
+    "Perrin", "Morin", "Mathieu", "Clement", "Gauthier", "Dumont", "Lopez", "Fontaine", "Chevalier", "Robin",
+    "Leclerc", "Giraud", "Lacroix", "Lecomte", "Lemaire", "Moulin",
+    "Marchand", "Renard", "Picard", "Gaillard", "Barbier", "Arnaud", "Meunier", "Caron", "Charpentier", "Dupuis",
+    "Leroux", "Dufour", "Boucher", "Fleury", "Lemoine", "Pelletier", "Lejeune", "Renaud", "Deschamps", "Bourgeois",
+    "Perez", "Brun", "Leclercq", "Guillot", "Collet", "Prevost", "Royer", "Colin", "Aubert", "Vidal",
+    "Benoit", "Pichon", "Lefort", "Philippe", "Gérard", "Brunet", "Carpentier", "Rey", "Maillard", "Baron",
+    "Berger", "Langlois", "Jacquet", "Lesage", "Cordier", "Aubry", "Humbert", "Bouvier", "Carre", "Olive"
+    
+]
+# Array of 50 male German names
+german_male_names = [
+    "Hans", "Klaus", "Wolfgang", "Jürgen", "Dieter", "Manfred", "Helmut", "Günter", "Peter", "Uwe",
+    "Horst", "Werner", "Gerhard", "Walter", "Heinrich", "Karl", "Michael", "Andreas", "Stefan",
+    "Frank", "Rainer", "Bernd", "Rolf", "Martin", "Jörg", "Heinz", "Matthias", "Ulrich", "Joachim",
+    "Ernst", "Hermann", "Otto", "Friedrich", "Erich", "Kurt", "Wilhelm", "Reinhard", "Volker", "Gerd",
+    "Hartmut", "Detlef", "Norbert", "Axel", "Holger", "Bernhard", "Christoph", "Udo", "Markus", "Sven",
+    "Thomas", "Johannes", "Lukas", "Felix", "Maximilian", "Alexander", "Daniel", "Tobias", "Sebastian", "Florian",
+    "Philipp", "Jan", "David", "Simon", "Benjamin", "Tim", "Niklas", "Fabian", "Jonas", "Moritz",
+    "Leon", "Elias", "Paul", "Luca", "Julian", "Max", "Finn", "Noah", "Emil", "Anton",
+    "Oskar", "Leo", "Theo", "Luis", "Ben", "Mats", "Liam", "Nico", "Tom", "Erik",
+    "Jannik", "Vincent", "Jonathan", "Lennard", "Hannes", "Marvin", "Linus", "Nils", "Robin", "Till"
+]
+# Array of 50 female German names
+german_female_names = [
+    "Anna", "Maria", "Ursula", "Elisabeth", "Monika", "Helga", "Ingrid", "Renate", "Gisela", "Christa",
+    "Erika", "Karin", "Brigitte", "Hannelore", "Gerda", "Inge", "Ilse", "Hildegard", "Elfriede", "Irmgard",
+    "Gertrud", "Marianne", "Edith", "Lieselotte", "Anneliese", "Sigrid", "Waltraud", "Elke", "Gerlinde", "Heike",
+    "Sabine", "Birgit", "Angelika", "Gabriele", "Petra", "Susanne", "Bärbel", "Claudia", "Ingeborg", "Christel",
+    "Margot", "Jutta", "Dagmar", "Ute", "Martina", "Andrea", "Stefanie", "Silvia", "Beate", "Cornelia",
+    "Sophie", "Julia", "Laura", "Katharina", "Christina", "Melanie", "Nicole", "Katrin", "Nadine", "Simone",
+    "Anja", "Tanja", "Manuela", "Anke", "Daniela", "Franziska", "Antje", "Stephanie", "Bettina", "Michaela",
+    "Sonja", "Britta", "Kirsten", "Bianca", "Yvonne", "Carolin", "Katja", "Annika", "Svenja", "Jasmin",
+    "Vanessa", "Janina", "Sabrina", "Miriam", "Verena", "Johanna", "Corinna", "Mareike", "Ramona", "Maren",
+    "Lena", "Sarah", "Lisa", "Leonie", "Hannah", "Lara", "Nele", "Lea", "Emma", "Mia"
+]
+# Array of 50 German family names
+german_family_names = [
+    "Müller", "Schmidt", "Schneider", "Fischer", "Weber", "Meyer", "Wagner", "Becker", "Schulz", "Hoffmann",
+    "Schäfer", "Koch", "Bauer", "Richter", "Klein", "Wolf", "Schröder", "Neumann", "Schwarz", "Zimmermann",
+    "Braun", "Krüger", "Hofmann", "Hartmann", "Lange", "Schmitt", "Werner", "Schmitz", "Krause", "Meier",
+    "Lehmann", "Schmid", "Schulze", "Maier", "Köhler", "Herrmann", "König", "Walter", "Mayer", "Huber",
+    "Kaiser", "Fuchs", "Peters", "Lang", "Scholz", "Möller", "Weiß", "Jung", "Hahn", "Schubert",
+    "Schumacher", "Vogel", "Friedrich", "Keller", "Günther", "Berger", "Frank", "Roth", "Beck", "Lorenz",
+    "Baumann", "Franke", "Albrecht", "Ludwig", "Winter", "Kraus", "Martin", "Schuster", "Krauss", "Steiner",
+    "Haas", "Jäger", "Otto", "Sommer", "Groß", "Seidel", "Heinrich", "Brandt", "Schreiber", "Graf",
+    "Schulte", "Dietrich", "Ziegler", "Engel", "Kühn", "Kuhn", "Pohl", "Horn", "Busch", "Thomas",
+    "Wolff", "Sauer", "Bergmann", "Pfeffer", "Vogt", "Ernst", "Seidl", "Hübner", "Beyer", "Hoppe"
+]
+
+names = set()
+
+names.update(french_male_names)
+names.update(french_female_names)
+names.update(french_family_names)
+names.update(german_male_names)
+names.update(german_female_names)
+names.update(german_family_names)
+
+from pymilvus.model.hybrid import BGEM3EmbeddingFunction
+import torch
+import numpy as np
+
+bge_m3_ef = BGEM3EmbeddingFunction(
+    model_name='BAAI/bge-m3', 
+    device='cpu',
+    return_sparse=False
+)
+
+def get_embedding(text):
+    with torch.cuda.amp.autocast():
+        return bge_m3_ef.encode_documents([text])["dense"][0]
+
+name_embeddings = []
+for n in names:
+    name_embeddings.append(get_embedding(n))
+    
+
+# Convert the list of embeddings to a numpy array
+name_embeddings_matrix = np.array(name_embeddings).T  # Transpose to make each embedding a column
+
+# Save the matrix as ch_names.npy
+np.save('ch_names.npy', name_embeddings_matrix)
+
+print(f"Matrix shape: {name_embeddings_matrix.shape}")
+print("Matrix saved as ch_names.npy")
